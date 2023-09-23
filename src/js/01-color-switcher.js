94179d1356e329fe2ab6663b07572908d59f1cbd
TaniaUdod <taniaudod28@gmail.com>
2023-09-23 22:47:14 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+btnStop.disabled = true;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStart() {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}
+
+function onStop() {
+    clearInterval(intervalId);
+    
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
